a46f7afbd694534829c7f81e5c2407e1d5c64fbc
siddharth-kp <siddharthkp33@gmail.com>
2026-04-04 01:03:23 +0530

    redis receivong logs

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 
 func main() {
 	// diff to create PR
-	fmt.Println("Starting DLPAS - Distributed Log Processing and Alerting System\n")
+	fmt.Println("Starting DLPAS - Distributed Log Processing and Alerting System")
 
 	// Start Service 1 (Log Producer)
 	service1Cmd := exec.Command("go", "run", "./service1/main.go")
@@ -46,7 +46,7 @@ func main() {
 	}
 	fmt.Println("✓ Service 3 (Processor) started")
 
-	fmt.Println("\nAll services running. Press Ctrl+C to stop.\n")
+	fmt.Println("All services running. Press Ctrl+C to stop.")
 
 	// Handle graceful shutdown
 	sigCh := make(chan os.Signal, 1)
